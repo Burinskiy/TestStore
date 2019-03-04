1baeb587a7633b4ded8ee907b110e9ddbb729f00
oleksandrburinskij <oleksandrburinskij@gmail.com>
2019-03-04 16:28:30 +0200

    updated

--- WebApplication1/Controllers/OrdersController.cs
+++ WebApplication1/Controllers/OrdersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,21 +13,29 @@ namespace WebApplication1.Controllers
     {
         // GET: Orders
         ApplicationDbContext db = new ApplicationDbContext();
+        [Authorize(Roles ="StoreOwner")]
         public ActionResult Index()
         {
-            IEnumerable<Order> orders = db.Orders;
+            IEnumerable<Order> orders = db.Orders.Include(p=>p.Carts);
             return View(orders);
         }
 
         // GET: Orders/Details/5
+        [Authorize(Roles = "StoreOwner")]
         public ActionResult Details(int id)
         {
             return View();
         }
 
         // GET: Orders/Create
-        public ActionResult SaveOrder(List<Cart> carts)
+        
+        public ActionResult SaveOrder()
         {
+            List<Cart> carts = (List<Cart>)Session["cart"];
+            foreach (Cart cart in carts)
+            {
+                db.Carts.Add(cart);
+            }
             Order order = new Order { CreateDate = DateTime.Now, UserId = User.Identity.GetUserId(), Carts = carts};
             db.Orders.Add(order);
             db.SaveChanges();
@@ -35,6 +44,7 @@ namespace WebApplication1.Controllers
 
         // POST: Orders/Create
         [HttpPost]
+        [Authorize(Roles = "StoreOwner")]
         public ActionResult Create(FormCollection collection)
         {
             try
@@ -50,6 +60,7 @@ namespace WebApplication1.Controllers
         }
 
         // GET: Orders/Edit/5
+        [Authorize(Roles = "StoreOwner")]
         public ActionResult Edit(int id)
         {
             return View();
@@ -57,6 +68,7 @@ namespace WebApplication1.Controllers
 
         // POST: Orders/Edit/5
         [HttpPost]
+        [Authorize(Roles = "StoreOwner")]
         public ActionResult Edit(int id, FormCollection collection)
         {
             try
@@ -72,6 +84,7 @@ namespace WebApplication1.Controllers
         }
 
         // GET: Orders/Delete/5
+        [Authorize(Roles = "StoreOwner")]
         public ActionResult Delete(int id)
         {
             return View();
@@ -79,6 +92,7 @@ namespace WebApplication1.Controllers
 
         // POST: Orders/Delete/5
         [HttpPost]
+        [Authorize(Roles = "StoreOwner")]
         public ActionResult Delete(int id, FormCollection collection)
         {
             try
